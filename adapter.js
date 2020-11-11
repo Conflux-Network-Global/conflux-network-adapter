ab54b3f40c2f12092d1f56ee5b70cb9e28b89cb2
aalu1418 <aalu1418@gmail.com>
2020-11-11 09:13:04 -0500

    updated to js-conflux-sdk v1 + linting fixes

--- adapter.js
+++ adapter.js
@@ -1,16 +1,15 @@
-const web3 = require("web3");
-const { Conflux } = require("js-conflux-sdk");
-const { Requester, Validator } = require("@chainlink/external-adapter");
-require("dotenv").config();
+const web3 = require('web3')
+const { Conflux } = require('js-conflux-sdk')
+const { Requester, Validator } = require('@chainlink/external-adapter')
+require('dotenv').config()
 
 const provider = new Conflux({
-  url: process.env.URL,
+  url: process.env.URL
   // logger: console, //JSON RPC call logging
-});
-const privateKey = process.env.PRIVATE_KEY;
-// const wallet = provider.Account({privateKey}); //v1 SDK
-const wallet = provider.Account(privateKey); //v0 SDK
-console.log("Fulfillment address: ", wallet.address);
+})
+const privateKey = process.env.PRIVATE_KEY
+const wallet = provider.wallet.addPrivateKey(privateKey)
+// console.log("Fulfillment address: ", wallet.address);
 
 const sendFulfillment = async (
   address,
@@ -18,41 +17,39 @@ const sendFulfillment = async (
   functionSelector,
   value
 ) => {
-  const dataPrefixBz = web3.utils.hexToBytes(dataPrefix);
-  const functionSelectorBz = web3.utils.hexToBytes(functionSelector);
-  const valueBz = web3.utils.hexToBytes(web3.utils.padLeft(web3.utils.numberToHex(value), 64));
-  const data = functionSelectorBz.concat(dataPrefixBz, valueBz);
+  const dataPrefixBz = web3.utils.hexToBytes(dataPrefix)
+  const functionSelectorBz = web3.utils.hexToBytes(functionSelector)
+  const valueBz = web3.utils.hexToBytes(web3.utils.padLeft(web3.utils.numberToHex(value), 64))
+  const data = functionSelectorBz.concat(dataPrefixBz, valueBz)
 
   const tx = {
     to: address,
-    // from: wallet.address, //v1 SDK
-    from: wallet, //v0 SDK
+    from: wallet,
     data: web3.utils.bytesToHex(data),
     storageLimit: 128,
     gas: 500000
-  };
+  }
 
-  // return await wallet.sendTransaction(tx).executed(); //v1 SDK
-  return await provider.sendTransaction(tx).executed(); //v0 SDK
-};
+  return await provider.sendTransaction(tx).executed()
+}
 
 const customParams = {
   // Use two sets of possible keys in case the node operator
   // is using a non-EI initiator where the primary keys are reserved.
-  address: ["address", "cfxAddress"],
-  dataPrefix: ["dataPrefix", "cfxDataPrefix"],
-  functionSelector: ["functionSelector", "cfxFunctionSelector"],
-  value: ["result", "value"]
-};
+  address: ['address', 'cfxAddress'],
+  dataPrefix: ['dataPrefix', 'cfxDataPrefix'],
+  functionSelector: ['functionSelector', 'cfxFunctionSelector'],
+  value: ['result', 'value']
+}
 
 const createRequest = (input, callback) => {
   // console.log(input);
-  const validator = new Validator(callback, input, customParams);
-  const jobRunID = validator.validated.id;
-  const address = validator.validated.data.address;
-  const dataPrefix = validator.validated.data.dataPrefix;
-  const functionSelector = validator.validated.data.functionSelector;
-  const value = validator.validated.data.value;
+  const validator = new Validator(callback, input, customParams)
+  const jobRunID = validator.validated.id
+  const address = validator.validated.data.address
+  const dataPrefix = validator.validated.data.dataPrefix
+  const functionSelector = validator.validated.data.functionSelector
+  const value = validator.validated.data.value
 
   const _handleResponse = tx => {
     // console.log(tx);
@@ -60,17 +57,17 @@ const createRequest = (input, callback) => {
       data: { result: tx.transactionHash },
       result: tx.transactionHash,
       status: 200
-    };
-    callback(response.status, Requester.success(jobRunID, response));
-  };
+    }
+    callback(response.status, Requester.success(jobRunID, response))
+  }
 
   const _handleError = err => {
-    callback(500, Requester.errored(jobRunID, err));
-  };
+    callback(500, Requester.errored(jobRunID, err))
+  }
 
   sendFulfillment(address, dataPrefix, functionSelector, value)
     .then(_handleResponse)
-    .catch(_handleError);
-};
+    .catch(_handleError)
+}
 
-module.exports = { createRequest: createRequest };
+module.exports = { createRequest: createRequest }
